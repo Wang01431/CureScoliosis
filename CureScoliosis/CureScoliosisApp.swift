4625e7b96a292761c846a4a674b4c9eeeb670156
王瀚哲 <wang01431@icloud.com>
2024-03-01 20:15:45 +0800

    Initial Commit

---
+++ CureScoliosis/CureScoliosisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CureScoliosisApp.swift
+//  CureScoliosis
+//
+//  Created by 王瀚哲 on 2024/3/1.
+//
+
+import SwiftUI
+
+@main
+struct CureScoliosisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
