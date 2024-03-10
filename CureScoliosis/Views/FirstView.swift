1d6c00da8e69db8018897690682c5957eac43503
王瀚哲 <wang01431@icloud.com>
2024-03-10 12:23:52 +0800

    Continue designing FirstView.

--- CureScoliosis/Views/FirstView.swift
+++ CureScoliosis/Views/FirstView.swift
@@ -9,43 +9,71 @@ import SwiftUI
 
 struct FirstView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Image(systemName: "house.fill")
-                    .foregroundColor(.accentColor)
-                
-                Text("Welcome back!")
-                    .fontWeight(.medium)
-            }
-            .font(.largeTitle)
-            
-            ZStack(alignment: .leading) {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color.accentColor.opacity(0.7))
-                
+        ScrollView {
+            VStack(alignment: .leading) {
                 HStack {
-                    Text("Check Off")
-                    Spacer()
-                    Image(systemName: "arrow.right")
+                    Image(systemName: "house.fill")
+                        .foregroundColor(.accentColor)
+                    
+                    Text("Welcome back, (nickname)!")
+                        .fontWeight(.medium)
                 }
-                .padding()
-            }
-            .frame(height: 50)
-            
-            ZStack(alignment: .leading) {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color.gray.opacity(0.1))
+                .font(.largeTitle)
                 
-                HStack {
-                    Text("Import")
-                    Spacer()
-                    Image(systemName: "arrow.right")
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.accentColor.opacity(0.4))
+                        .shadow(radius: 8)
+                    
+                    HStack {
+                        Text("Check Off")
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                    }
+                    .padding()
                 }
-                .padding()
+                .frame(height: 50)
+                
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.gray.opacity(0.1))
+                        .shadow(radius: 8)
+                    
+                    HStack {
+                        Text("Import")
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                    }
+                    .padding()
+                }
+                .frame(height: 50)
+                
+                VStack(alignment: .leading) {
+                    Text("Suggestions")
+                        .font(.headline)
+                    
+                    VStack {
+                        ForEach(0..<5) { index in
+                            ZStack(alignment: .leading) {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.gray.opacity(0.1))
+                                    .shadow(radius: 8)
+                                
+                                HStack {
+                                    Text("Suggestion \(index)")
+                                    Spacer()
+                                    Image(systemName: "arrow.right")
+                                }
+                                .padding()
+                            }
+                            .frame(height: 50)
+                        }
+                    }
+                }
+                .padding(.top)
+                
+                Spacer()
             }
-            .frame(height: 50)
-            
-            Spacer()
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
